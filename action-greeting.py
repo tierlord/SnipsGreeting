529b9e769113a637893165ca5030c26cf7e7a5bf
tierlord <xtra-j@web.de>
2019-04-24 17:51:55 +0200

    Initial commit

---
+++ action-greeting.py
@@ -0,0 +1,24 @@
+from random import randint
+from hermes_python.hermes import Hermes
+
+
+def greet (hermes, message):
+    sentences = [
+    "Schön, dass du da bist.",
+    "Ich habe mich schon gefragt, wo du bleibst.",
+    "Willkommen zurück",
+    "Ich grüße dich",
+    "Ich hoffe, du hattest Spaß",
+    "Erzähl mir von deinem Tag!",
+    "Ich habe hier auf dich gewartet"
+    ]
+
+    generated_int = randint(0, len(sentences)-1)
+    msg = sentences[generated_int]
+
+    hermes.publish_end_session(message.session_id, msg)
+
+with Hermes("localhost:1883") as h:
+    h \
+        .subscribe_intent("tierlord:Begrüßung", greet) \
+        .start()
